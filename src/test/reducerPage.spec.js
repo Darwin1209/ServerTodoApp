4acd349df8395a984054654d2ac3216ae04e1cce
Денис <denis.darwin12@gmail.com>
2019-11-29 00:19:40 +0300

    Протестировали reducer

--- src/test/reducerPage.spec.js
+++ src/test/reducerPage.spec.js
@@ -10,5 +10,44 @@ describe('pageRecucer', () => {
             ...initialState,
             todoData: [1, 2, 3],
         });
+    });
+    it('test deleteItem', () => {
+        const action = {
+            type: 'DELETE_ITEM',
+            payload: [1, 2, 3],
+        }
+        expect(pageReducer(initialState, action)).toEqual({
+            ...initialState,
+            todoData: [1, 2, 3],
+        });
+    });
+    it('test toogle', () => {
+        const action = {
+            type: 'TOOGLE',
+            payload: [1, 2, 3],
+        }
+        expect(pageReducer(initialState, action)).toEqual({
+            ...initialState,
+            todoData: [1, 2, 3],
+        });
+    });
+    it('test fetchTodoSuccess', () => {
+        const action = {
+            type: 'FETCH_TODO_SUCCESS',
+            payload: [1, 2, 3],
+        }
+        expect(pageReducer(initialState, action)).toEqual({
+            ...initialState,
+            todoData: [1, 2, 3],
+        });
+    });
+    it('test default', () => {
+        const action = {
+            type: 'Sfaefae',
+            payload: [1, 2, 3],
+        }
+        expect(pageReducer(initialState, action)).toEqual({
+            ...initialState
+        });
     })
 })
